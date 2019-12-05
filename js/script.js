ec9b635c8db916ebde47a64937db4ea0bebc0188
Tikondra <tikondra47@gmail.com>
2019-12-05 14:23:15 +0300

    slide

--- js/script.js
+++ js/script.js
@@ -12,3 +12,28 @@ $(function() {
  	dots: false
  });
 });
+
+function indexJs() {
+  let img = [
+      '../img/full-img1.jpg',
+      '../img/full-img2.jpg',
+      '../img/full-img3.jpg'];
+  let catalog = document.querySelector('.catalog__box');
+  let item = catalog.querySelectorAll('.catalog__item');
+  let fullImg = catalog.querySelector('.catalog__full-img');
+
+  console.log(item[2]);
+
+  for (let i = 0; i < item.length; i++) {
+
+    item[i].addEventListener('click', function () {
+      for (let i = 0; i < item.length; i++) {
+        item[i].classList.remove('active');
+      }
+      item[i].classList.add('active');
+      fullImg.src = img[i];
+    });
+  }
+};
+
+indexJs();
