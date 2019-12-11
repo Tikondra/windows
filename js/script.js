83eb12f870251afdec1bec560ca894baa6cf3ce7
Tikondra <tikondra47@gmail.com>
2019-12-11 15:53:30 +0400

    price

--- js/script.js
+++ js/script.js
@@ -41,8 +41,13 @@ function price() {
   let prew = price.querySelectorAll('.slider-price__link--prew');
   let status = price.querySelector('.status-bar');
   let statusText = price.querySelector('.status-bar__text');
+  let inputBox = price.querySelectorAll('.slider-price2__item');
+  let inputs = price.querySelectorAll('.slider-price2__select');
+  let tipCommunication = price.querySelector('.slider-price3__select--communication');
+  let inputBox2 = price.querySelectorAll('.slider-price3__item');
+  let inputs2 = price.querySelectorAll('.js__input');
 
-  for (let i = 0; i < next.length-1; i++) {
+  for (let i = 0; i < next.length; i++) {
 
     next[i].addEventListener('click', function () {
 
@@ -70,6 +75,110 @@ function price() {
       statusText.textContent = 'Вопрос ' + [i+1] + ' из 3';
     });
   }
+
+  inputs[0].addEventListener('change', function () {
+
+    var selind = inputs[0].options.selectedIndex;
+    var txt = inputs[0].options[selind].text;
+    var val = inputs[0].options[selind].value;
+
+    for (let i = 0; i < inputs.length; i++) {
+      inputs[i].disabled = false;
+    };
+
+
+    if (val == 'wood') {
+          inputBox[1].classList.add('slider-price2__item--hide');
+          inputBox[3].classList.add('slider-price2__item--hide');
+          inputBox[4].classList.remove('slider-price2__item--hide');
+          inputBox[5].classList.remove('slider-price2__item--hide');
+          inputBox[6].classList.add('slider-price2__item--hide');
+          inputBox[7].classList.add('slider-price2__item--hide');
+    } else
+
+    if (val == 'PVH') {
+          inputBox[1].classList.remove('slider-price2__item--hide');
+          inputBox[3].classList.remove('slider-price2__item--hide');
+          inputBox[4].classList.add('slider-price2__item--hide');
+          inputBox[5].classList.add('slider-price2__item--hide');
+          inputBox[6].classList.add('slider-price2__item--hide');
+          inputBox[7].classList.add('slider-price2__item--hide');
+    } else
+
+    if (val == 'wood-al') {
+          inputBox[1].classList.add('slider-price2__item--hide');
+          inputBox[3].classList.add('slider-price2__item--hide');
+          inputBox[4].classList.remove('slider-price2__item--hide');
+          inputBox[5].classList.add('slider-price2__item--hide');
+          inputBox[6].classList.add('slider-price2__item--hide');
+          inputBox[7].classList.add('slider-price2__item--hide');
+    } else
+
+    if (val == 'al') {
+          inputBox[1].classList.add('slider-price2__item--hide');
+          inputBox[3].classList.add('slider-price2__item--hide');
+          inputBox[4].classList.add('slider-price2__item--hide');
+          inputBox[5].classList.add('slider-price2__item--hide');
+          inputBox[6].classList.remove('slider-price2__item--hide');
+          inputBox[7].classList.remove('slider-price2__item--hide');
+    }
+
+
+    for (let j = 0; j < inputBox.length; j++) {
+
+      if (inputBox[j].classList.contains('slider-price2__item--hide')) {
+        inputs[j].disabled = "disabled";
+      };
+    };
+  });
+
+  tipCommunication.addEventListener('change', function () {
+
+    var ind = tipCommunication.options.selectedIndex;
+    var text = tipCommunication.options[ind].text;
+    var value = tipCommunication.options[ind].value;
+
+    for (let i = 0; i < inputs2.length; i++) {
+      inputs2[i].disabled = false;
+    };
+
+
+    if (value == 'mob') {
+          inputBox2[4].classList.remove('slider-price3__item--hide');
+          inputBox2[5].classList.add('slider-price3__item--hide');
+          inputBox2[6].classList.add('slider-price3__item--hide');
+          inputBox2[7].classList.add('slider-price3__item--hide');
+    } else
+
+    if (value == 'whats') {
+          inputBox2[4].classList.add('slider-price3__item--hide');
+          inputBox2[5].classList.remove('slider-price3__item--hide');
+          inputBox2[6].classList.add('slider-price3__item--hide');
+          inputBox2[7].classList.add('slider-price3__item--hide');
+    } else
+
+    if (value == 'vk') {
+          inputBox2[4].classList.add('slider-price3__item--hide');
+          inputBox2[5].classList.add('slider-price3__item--hide');
+          inputBox2[6].classList.remove('slider-price3__item--hide');
+          inputBox2[7].classList.add('slider-price3__item--hide');
+    } else
+
+    if (value == 'inst') {
+          inputBox2[4].classList.add('slider-price3__item--hide');
+          inputBox2[5].classList.add('slider-price3__item--hide');
+          inputBox2[6].classList.add('slider-price3__item--hide');
+          inputBox2[7].classList.remove('slider-price3__item--hide');
+    }
+
+
+    for (let j = 0; j < inputs2.length; j++) {
+
+      if (inputBox2[j+4].classList.contains('slider-price3__item--hide')) {
+        inputs2[j].disabled = "disabled";
+      };
+    };
+  });
 };
 
 if (document.querySelector('.catalog__box')) {
